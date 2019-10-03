07616519f9503447cf070fd7a146ced3a7b5c5e2
terence1991 <cplterencehawes@gmail.com>
2019-10-03 11:22:53 -0400

    navbar set up

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="navbar">
+    <div class="nav">
+     <router-link>
+      <span class="nav-title">Smoithies Magic Smoothies </span>
+     </router-link>
+     <a href="" class="btn-floating btn-large halfway-fab pink">
+       <router-link>
+        <i class="material-icons">add</i>       
+       </router-link>
+     </a>
+    </div>
+  </div>
+</template>
+
+<script>
+ export default{
+   name: "NavBar",
+   data() {
+     return
+   }
+ }
+</script>
+
+<style>
+.navbar nav {
+  padding: 0 20px;
+  
+}
+</style>
