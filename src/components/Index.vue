077db321e4e9d0416578ef543e752396361d9538
terence1991 <cplterencehawes@gmail.com>
2019-10-18 12:09:05 -0400

    styling added

---
+++ src/components/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="smothie in smoothies" :key="smothie.id">
+    
+    
+    </div>
+  </div>
+</template>
+
+
+
+
+<script>
+export default {
+  name:  'index', 
+  data () {
+    return {
+      smoothies: [
+        {title: 'Ninja-Brew', slug: 'ninja-brew', ingrediants: ['bannas', 'coffe', 'milk'], id: '1'},
+        {title: 'Magic-Mushroom', slug: 'magic-mushroom', ingrediants: ['mush'], id: '2'},
+      ]
+    }
+  }
+
+}
+</script>
